97fb404110bc4f6c9cfc01114e2c81294fa8cc02
shafeeque <shafeeqsha06@gmail.com>
2025-12-20 10:25:09 +0530

    refactor: refactored or removed few dtos definitions

--- src/application/dto/auth/verify-user.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEmail } from 'class-validator';
-import { IUser } from '@/domain/interfaces/user';
-
-export default class VerifyUserDto implements Partial<IUser> {
-  @IsEmail({}, { message: 'Invalid email format' })
-  email!: string;
-}
