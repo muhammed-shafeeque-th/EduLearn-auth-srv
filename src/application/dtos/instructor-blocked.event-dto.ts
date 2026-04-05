1f85902bdbb29e82e13f94c413a0cd53dd3c3a8f
shafeeque <shafeeqsha06@gmail.com>
2026-04-05 18:03:09 +0530

    feat(dto): update user dto to align with user entity change, changed user.role to user.roles

---
+++ src/application/dtos/instructor-blocked.event-dto.ts
@@ -0,0 +1,44 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import BaseEventDto from './base-event.dto';
+import { UserStatus, UserRoles } from '@/domain/entity/user';
+import { BaseEvent } from '@/domain/events/types/base-event';
+
+export type InstructorBlockedEvent = BaseEvent<{
+  userId: string;
+  email: string;
+  role: UserRoles;
+  status: UserStatus.BLOCKED;
+  firstName?: string;
+  avatar?: string;
+  roles?: UserRoles[];
+  roleStatus?: Record<UserRoles, string>;
+}>;
+
+export class InstructorBlockedPayload {
+  @IsString()
+  @IsNotEmpty({ message: 'userId is required' })
+  userId!: string;
+
+  @IsEmail({}, { message: 'Invalid email format' })
+  @IsNotEmpty({ message: 'email is required' })
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'status is required' })
+  status!: string;
+
+  @IsOptional()
+  @IsString()
+  firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  avatar?: string;
+
+  @IsOptional()
+  roles?: UserRoles[];
+
+  @IsOptional()
+  roleStatus?: Record<UserRoles, string>;
+}
+export default class InstructorBlockedDto extends BaseEventDto<InstructorBlockedPayload> {}
---
+++ src/application/dtos/instructor-unblocked.event-dto.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import { IsEmail, IsNotEmpty, IsString, IsOptional } from 'class-validator';
+import BaseEventDto from './base-event.dto';
+import { BaseEvent } from '@/domain/events/types/base-event';
+
+export interface InstructorUnblockedEvent
+  extends BaseEvent<{
+    userId: string;
+    email: string;
+    role: string;
+    status: string;
+    firstName?: string;
+    avatar?: string;
+    roles?: string[];
+    roleStatus?: Record<string, string>;
+  }> {}
+
+export class InstructorUnblockedDtoPayload {
+  @IsString()
+  @IsNotEmpty({ message: 'userId is required' })
+  userId!: string;
+
+  @IsEmail({}, { message: 'Invalid email format' })
+  @IsNotEmpty({ message: 'email is required' })
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'status is required' })
+  status!: string;
+
+  @IsOptional()
+  @IsString()
+  firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  avatar?: string;
+
+  @IsOptional()
+  roles?: string[];
+
+  @IsOptional()
+  roleStatus?: Record<string, string>;
+}
+
+export default class InstructorUnblockedDto extends BaseEventDto<InstructorUnblockedDtoPayload> {}
