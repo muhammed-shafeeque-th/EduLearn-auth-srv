fec6ae08a4a6ddc31d12c753fdf2614150076e92
shafeeque <shafeeqsha06@gmail.com>
2026-04-05 18:02:20 +0530

    refactor(adaptor): rename user block/unblock into blockAccount and unBlockAccount use-cases

--- src/application/adaptors/unblock-account.interface.ts
+++ src/application/adaptors/unblock-account.interface.ts
@@ -1,16 +1,16 @@
-import UserUnblockedDto from '../dtos/unblock-user.event-dto';
+import AccountUnblockedDto from '../dtos/instructor-unblocked.event-dto';
 
 /**
  * Interface for the UserUnblocked use case.
  *
  * Provides a contract for unblocking a user in the authentication system.
  */
-export interface IUserUnblockedUseCase {
+export interface IAccountUnblockedUseCase {
   /**
    * Unblocks a user specified by the given dto.
    *
-   * @param {UserUnblockedDto} dto - Data transfer object containing user information to be unblocked.
+   * @param {AccountUnblockedDto} dto - Data transfer object containing user information to be unblocked.
    * @returns {Promise<void>} A promise that resolves when the user has been unblocked.
    */
-  execute(dto: UserUnblockedDto): Promise<void>;
+  execute(dto: AccountUnblockedDto): Promise<void>;
 }
