b4fe52b5284a7cf81b9773d9813ec212974cdc38
shafeeque <shafeeqsha06@gmail.com>
2026-04-06 10:33:10 +0530

    feat(error): update error types to extend from updated abstract baseError type

--- src/shared/errors/env-not-found.error.ts
+++ src/shared/errors/env-not-found.error.ts
@@ -1,18 +1,12 @@
 import { BaseError } from './base-error';
-import { ErrorCodes } from './error-codes/error-codes';
-import { StatusCodes } from './error-codes/error-status-codes';
+import { ErrorCode } from './error-codes/error-codes';
 
 export class EnvNotFoundError extends BaseError {
-  public errorCode: ErrorCodes.ENV_LOAD_ERROR;
-  public statusCode: StatusCodes.INTERNAL_ERROR;
   private env: string;
 
   constructor(env: string) {
-    super("can't load env " + env);
+    super(ErrorCode.INTERNAL, "can't load env " + env, 'ENV_NOT_FOUND');
     this.env = env;
-
-    this.statusCode = StatusCodes.INTERNAL_ERROR;
-    this.errorCode = ErrorCodes.ENV_LOAD_ERROR;
   }
   serializeErrors(): { message: string; field?: string }[] {
     return [{ message: 'Error while loading env' }];
