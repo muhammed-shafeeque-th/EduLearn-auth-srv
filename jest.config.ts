18b5a8cb070986f5c26c2664854e87a00224b369
shafeeque <shafeeqsha06@gmail.com>
2026-06-19 21:44:03 +0530

    test: add jest config file

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.spec.ts', '**/*.test.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^test/(.*)$': '<rootDir>/test/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.test.json',
+      },
+    ],
+  },
+};
